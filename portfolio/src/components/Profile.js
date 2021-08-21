9015e1dd08641a8e9cb66e3c3040d4bc379cfe5f
Orlando <galunggong14@gmail.com>
2021-08-21 11:02:51 -0700

    portfolio

---
+++ portfolio/src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>Orlando Nava</p>
+        </div>
+    );
+}
+
+export default Profile;
