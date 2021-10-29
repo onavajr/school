dc844911a0e97406b6b54f90be81574c8edce55c
Orlando <galunggong14@gmail.com>
2021-10-29 12:31:03 -0700

    new application

---
+++ FirstReact/firstreact/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Things i need to do:</h1>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mountain Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
